9cfd6a76c251b656a08a5d72e868e388a834a464
Prathamesh Sirdesai <sirdesai.work@gmail.com>
2023-06-18 21:11:18 +0530

    Menu, functions added

--- enquiry.py
+++ enquiry.py
@@ -1,67 +1,82 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def submit_enquiry():
-    name = entry_name.get()
-    email = entry_email.get()
-    phone = entry_phone.get()
-    course = entry_course.get()
-    message = entry_message.get("1.0", tk.END)
+def run():
+    def submit_enquiry():
+        name = entry_name.get()
+        email = entry_email.get()
+        phone = entry_phone.get()
+        course = entry_course.get()
+        message = entry_message.get("1.0", tk.END)
 
-    # Validate the inputs (you can add your own valid logic here)
+        # Validate the inputs (you can add your own valid logic here)
 
-    # Display a confirmation message box
-    messagebox.showinfo("Enquiry Submitted", "Thank you for your enquiry!")
+        # Display a confirmation message box
+        messagebox.showinfo("Enquiry Submitted", "Thank you for your enquiry!")
 
-    # Save the data to a file or database
-    save_enquiry(name, email, phone, course, message)
+        # Save the data to a file or database
+        save_enquiry(name, email, phone, course, message)
 
-def save_enquiry(name, email, phone, course, message):
-    # Here we can implement the code to save the enquiry data
-    # to a file or database
-    print("\nEnquiry Details:")
-    print("Name:", name)
-    print("Email:", email)
-    print("Phone number:", phone)
-    print("Course of interest:", course)
-    print("Message:", message)
+    def save_enquiry(name, email, phone, course, message):
+        # Here we can implement the code to save the enquiry data
+        # to a file or database
+        print("\nEnquiry Details:")
+        print("Name:", name)
+        print("Email:", email)
+        print("Phone number:", phone)
+        print("Course of interest:", course)
+        print("Message:", message)
 
-# Create the main window
-window = tk.Tk()
-window.geometry('800x400')
-window.title("College Enquiry Form")
-window.configure(bg="lavender")
-label_name = tk.Label(window, text="COLLEGE ENQUIRY FORM", font='ALGERIAN 30',bg="lavender")
-label_name.pack()
-# Create labels and entry fields
-label_name = tk.Label(window, text="Name:",bg="lavender")
-label_name.place(x=10,y=60)
-entry_name = tk.Entry(window)
-entry_name.place(x=80,y=60)
+    # def open_redirect_window():
+    #     redirect_window = tk.Toplevel(window)
+    #     redirect_window.geometry('400x200')
+    #     redirect_window.title("Redirect Window")
+    #     redirect_window.configure(bg="lavender")
 
-label_email = tk.Label(window, text="Email:",bg="lavender")
-label_email.place(x=10,y=100)
-entry_email = tk.Entry(window)
-entry_email.place(x=80,y=100)
+    #     label_redirect = tk.Label(redirect_window, text="This is a redirect window.", font='ALGERIAN 20', bg="lavender")
+    #     label_redirect.pack()
 
-label_phone = tk.Label(window, text="Phone \nnumber:",bg="lavender")
-label_phone.place(x=10,y=135)
-entry_phone = tk.Entry(window)
-entry_phone.place(x=80,y=150)
+    # Create the main window
+    window = tk.Tk()
+    window.geometry('800x400')
+    window.title("College Enquiry Form")
+    window.configure(bg="lavender")
+    label_name = tk.Label(window, text="COLLEGE ENQUIRY FORM", font='ALGERIAN 30',bg="lavender")
+    label_name.pack()
+    # Create labels and entry fields
+    label_name = tk.Label(window, text="Name:",bg="lavender")
+    label_name.place(x=10,y=60)
+    entry_name = tk.Entry(window)
+    entry_name.place(x=80,y=60)
 
-label_course = tk.Label(window, text="Course \n of interest:",bg="lavender")
-label_course.place(x=10,y=180)
-entry_course = tk.Entry(window)
-entry_course.place(x=80,y=195)
+    label_email = tk.Label(window, text="Email:",bg="lavender")
+    label_email.place(x=10,y=100)
+    entry_email = tk.Entry(window)
+    entry_email.place(x=80,y=100)
 
-label_message = tk.Label(window, text="Message:",bg="lavender")
-label_message.place(x=10,y=240)
-entry_message = tk.Text(window, height=5)
-entry_message.place(x=80,y=240)
+    label_phone = tk.Label(window, text="Phone \nnumber:",bg="lavender")
+    label_phone.place(x=10,y=135)
+    entry_phone = tk.Entry(window)
+    entry_phone.place(x=80,y=150)
 
-# Create a submit button
-submit_button = tk.Button(window, text="Submit", command=submit_enquiry,font='arial 15')
-submit_button.place(x=640,y=350)
+    label_course = tk.Label(window, text="Course \n of interest:",bg="lavender")
+    label_course.place(x=10,y=180)
+    entry_course = tk.Entry(window)
+    entry_course.place(x=80,y=195)
+
+    label_message = tk.Label(window, text="Message:",bg="lavender")
+    label_message.place(x=10,y=240)
+    entry_message = tk.Text(window, height=5)
+    entry_message.place(x=80,y=240)
+
+    # Create a submit button
+    submit_button = tk.Button(window, text="Submit", command=submit_enquiry,font='arial 15')
+    submit_button.place(x=640,y=350)
+
+    # # Create a redirect button
+    # redirect_button = tk.Button(window, text="Redirect", command=open_redirect_window, font='arial 15')
+    # redirect_button.place(x=540, y=350)
 
 
-window.mainloop()
+
+    window.mainloop()
---
+++ menu.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import tkinter as tk
+import admission as ad
+import enquiry as enq
+
+def formAdmi():
+    ad.run()
+def formEnq():
+    enq.run()
+def formMarks():
+    pass
+def formFeed():
+    pass
+
+root = Tk()
+root.geometry('300x250')
+root.title('Trial')
+root.resizable(False, False)
+
+Button(root, text="Addmission", bd=2, font=30, height=2, command=formAdmi, bg="lightblue").pack(fill=tk.X)
+Button(root, text="Enquiry", bd=2, font=30, height=2, command=formEnq, bg="pink").pack(fill=tk.X)
+Button(root, text="MarksSheet", bd=2, font=30, height=2, command=formMarks, bg="orange").pack(fill=tk.X)
+Button(root, text="Feedback", bd=2, font=30, height=2, command=formFeed, bg="lightgreen").pack(fill=tk.X)
+
+
+root.mainloop()
